46bb2b8425a730857f10b3d3b1181ba4cdb29a90
Tim Blackwell <timmblackwell@gmail.com>
2013-12-24 17:16:56 +0000

    Start node canConnect updated
    Only returns true if no other connections exist

--- src/tb/jhdfsm/figure/StartNode.java
+++ src/tb/jhdfsm/figure/StartNode.java
@@ -6,7 +6,7 @@ import java.awt.Point;
 import java.awt.Rectangle;
 import java.util.Vector;
 
-import tb.jhdfsm.connector.NodeConnector;
+import tb.jhdfsm.connector.StartNodeConnector;
 import CH.ifa.draw.framework.ConnectionFigure;
 import CH.ifa.draw.framework.Handle;
 import CH.ifa.draw.handle.ConnectionHandle;
@@ -20,14 +20,24 @@ public class StartNode extends CircleFigure{
 	private static final long serialVersionUID = -8363466797940835974L;
 
     public boolean endNode;
+    private boolean canConnect = true;
 
 	public StartNode() {
 		super();
 		this.setAttribute("FillColor", Color.BLACK);
 		this.setAttribute("FrameColor", Color.BLACK);
 	}
-    
-    
+
+
+	@Override
+	public boolean canConnect() {
+		return canConnect;
+	}
+	
+	public void connected(boolean figureConnected) {
+		this.canConnect = !figureConnected;
+	}
+	
 	@Override
 	public void basicDisplayBox(Point origin, Point corner) {
     	centerPoint = origin;
@@ -52,7 +62,7 @@ public class StartNode extends CircleFigure{
 	@Override
 	public Vector<Handle> handles() {
         Vector<Handle> handles = new Vector<Handle>();
-        ConnectionFigure prototype = new NodeConnector();
+        ConnectionFigure prototype = new StartNodeConnector();
         handles.addElement(new ConnectionHandle(this, RelativeLocator.center(), prototype));
         return handles;
     }
