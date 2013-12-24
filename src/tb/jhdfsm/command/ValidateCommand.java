5b38d94470b7ccbac160707883ca475f77276708
Tim Blackwell <timmblackwell@gmail.com>
2013-12-24 20:58:48 +0000

    Checking each node has 2 connections

--- src/tb/jhdfsm/command/ValidateCommand.java
+++ src/tb/jhdfsm/command/ValidateCommand.java
@@ -1,7 +1,14 @@
 package tb.jhdfsm.command;
 
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import tb.jhdfsm.connector.NodeConnector;
+import tb.jhdfsm.connector.StartNodeConnector;
+import tb.jhdfsm.figure.NodeFigure;
 import tb.jhdfsm.figure.StartNode;
 import CH.ifa.draw.command.Command;
 import CH.ifa.draw.framework.Drawing;
@@ -22,21 +29,80 @@ private DrawingView fView;
 		// TODO Auto-generated method stub
 		Drawing fDrawing = fView.drawing();
 		Enumeration<Figure> figures = fDrawing.figures();
-		boolean startNodePresent = false;
+		
+		List<StartNode> startNodes = new ArrayList<StartNode>();
+		List<StartNodeConnector> startNodeConnector = new ArrayList<StartNodeConnector>();
+
+		List<NodeFigure> nodes = new ArrayList<NodeFigure>();
+		List<NodeConnector> nodeConnectors = new ArrayList<NodeConnector>();
+		
+		Figure figure;
 		
 		while (figures.hasMoreElements()) {
-			if (figures.nextElement() instanceof StartNode) {
-				if (startNodePresent) {
-					System.out.println("Too many start nodes");
-				} else {
-					startNodePresent = true;
+			figure = figures.nextElement();
+			if (figure instanceof StartNode) {
+				startNodes.add((StartNode)figure);
+			} else if (figure instanceof NodeFigure) {
+				nodes.add((NodeFigure)figure);
+			} else if (figure instanceof NodeConnector) {
+				nodeConnectors.add((NodeConnector)figure);
+			} else if (figure instanceof StartNodeConnector) {
+				startNodeConnector.add((StartNodeConnector)figure);
+			}
+		}
+		
+		String errors = "";
+		
+		switch (startNodes.size()) {
+			case 0 : {
+				errors += "Missing start node";
+				break;
+			}
+			case 1 : {
+				break;
+			}
+			default : {
+				errors += "Too many start nodes";
+				break;
+			}
+		}
+		
+		Map<NodeFigure,ArrayList<NodeConnector>> nodeConnectorHash = new HashMap<NodeFigure,ArrayList<NodeConnector>>();
+		
+		for (NodeFigure node : nodes) {
+			nodeConnectorHash.put(node, new ArrayList<NodeConnector>());
+			for (NodeConnector nodeConnector : nodeConnectors) {
+				if (nodeConnector.startFigure().equals(node)){
+					nodeConnectorHash.get(node).add(nodeConnector);
 				}
 			}
 		}
 		
-		if (!startNodePresent) {
-			System.out.println("Missing start node");
+		for (NodeFigure node : nodes) {
+			switch (nodeConnectorHash.get(node).size()) {
+				case 0 : {
+					errors += "Missing node connections";
+					break;
+				}
+				case 1 : {
+					errors += "Missing node connections";
+					break;
+				}
+				case 2 : {
+					break;
+				}
+				default: {
+					errors += "Too many node connections";
+					break;
+				}
+			}
 		}
+		
+		if (errors.isEmpty()) {
+			errors += "Valid";
+		}
+		
+		System.out.println(errors);
 	}
 
 }
